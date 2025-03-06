aaf481ad532274ea125bfa16959f4977ba201e19
Joshisaurio <joshmanjarrez13@gmail.com>
2025-03-06 13:14:10 -0600

    Removed alerts

--- PlasmaTanks/script.js
+++ PlasmaTanks/script.js
@@ -2,13 +2,13 @@ console.log("Loaded script v0.3")
 
 window.onload = function(){
     console.log("Window loaded")
-    alert("Window loaded")
+    //alert("Window loaded")
     var iframe = document.getElementById("iframe");
     var fullbutton = document.getElementById("fullscreenButton");
 
     fullbutton.onclick = function() {
         console.log("Requested fullscreen")
-        alert("Game should go fullscreen. Ignore if it doesn't")
+        //alert("Game should go fullscreen. Ignore if it doesn't")
          iframe.requestFullscreen();
      }
  };
