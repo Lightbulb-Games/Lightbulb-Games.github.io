72e5895f5340485142fd22612829b3012cbdfed3
Joshisaurio <joshmanjarrez13@gmail.com>
2025-03-06 11:45:38 -0600

    Log

--- PlasmaTanks/script.js
+++ PlasmaTanks/script.js
@@ -2,7 +2,8 @@ window.onload = function(){
     var iframe = document.getElementById("iframe");
     var fullbutton = document.getElementById("fullscreenButton");
 
-     fullbutton.onclick = function() {
+    fullbutton.onclick = function() {
+        console.log("Requested fullscreen")
          iframe.requestFullscreen();
      }
  };
