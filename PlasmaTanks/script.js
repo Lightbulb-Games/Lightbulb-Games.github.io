2b193c686b10fa67d379aaae7b103d67fff010e4
Joshisaurio <joshmanjarrez13@gmail.com>
2025-03-06 11:00:14 -0600

    Tweaks

---
+++ PlasmaTanks/script.js
@@ -0,0 +1,9 @@
+let iframe = document.getElementById("iframe");
+let toggleButton = document.getElementById("fullscreenButton");
+
+console.log("Script loaded")
+
+toggleButton.onclick = function()
+            {
+                console.log("Fullscreen clicked");
+            };
